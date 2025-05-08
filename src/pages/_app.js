b9d181b61e91be8eb8a38d854af7a4e358bc477e
aleksey <azhlbn@gmail.com>
2025-05-08 23:12:54 +0300

    TotemFactory deployment

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,16 +1,14 @@
-import '../styles/globals.css';
-import { ApolloProvider } from '@apollo/client';
-import { apolloClient } from '../utils/apollo';
+import '../styles/basic.css';
 import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
   return (
-    <ApolloProvider client={apolloClient}>
+    <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Totem Creator</title>
       </Head>
       <Component {...pageProps} />
-    </ApolloProvider>
+    </>
   );
 }
